5f05a811ab55be9cfc00f7cec4691d3a27b9ce92
BlockG2500 <114137384+BlockG2500@users.noreply.github.com>
2023-07-09 10:11:04 +0000

    v

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ fetch('/api/applications')
       .then(response => response.json())
       .then(data => {
         console.log(data);
+        alert(data);
       })
       .catch(error => {
         console.error('Error:', error);
